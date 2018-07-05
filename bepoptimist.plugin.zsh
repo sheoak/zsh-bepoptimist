be109fbb81c9c12dcebb75753811dfd7b35decfd
Sheoak <gomunochikin@gmail.com>
2018-07-05 23:02:39 +0200

    Add bepo mappings

---
+++ bepoptimist.plugin.zsh
@@ -0,0 +1,6 @@
+
+# remap homerow for vim mode
+bindkey -M vicmd "s" up-line-or-beginning-search
+bindkey -M vicmd "t" down-line-or-beginning-search
+bindkey -M vicmd "c" vi-backward-char
+bindkey -M vicmd "r" vi-forward-char
